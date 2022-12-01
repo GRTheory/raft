3482aa98baf63224e8dac6ab027ae4536c58619d
zhangsan33 <8546260+zhangsan3356@user.noreply.gitee.com>
2022-12-01 14:53:51 +0800

    use the adviced params

--- server.go
+++ server.go
@@ -1,6 +1,9 @@
 package raft
 
 import (
+	"fmt"
+	"io"
+	"os"
 	"path"
 	"sync"
 	"time"
@@ -73,15 +76,23 @@ func (s *server) LogPath() string {
 }
 
 // Retrieves the current state of the server.
-func (s *server) State() string{
+func (s *server) State() string {
 	s.mutex.RLock()
 	defer s.mutex.Unlock()
 	return s.state
 }
 
 // Retrieves the current term of the server.
-func( s *server) Term() uint64{
+func (s *server) Term() uint64 {
 	s.mutex.RLock()
 	defer s.mutex.RUnlock()
 	return s.currentTerm
+}
+
+// Set the commitIndex at the head of the log file to the current
+// commit Index. This should be called after obtained a log lock
+func (l *Log) flushCommitIndex() {
+	l.file.Seek(0, io.SeekStart)
+	fmt.Fprintf(l.file, "%8x\n", l.commitIndex)
+	l.file.Seek(0, io.SeekEnd)
 }
